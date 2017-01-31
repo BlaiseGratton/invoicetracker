133690b369e44c953cbcbbf43ccb1aa8d8d6f7e4
Blaise Gratton <blaisegratton@gmail.com>
2017-01-30 19:34:03 -0800

    code for testing timestamps

---
+++ InvoiceItemTracker/InvoiceItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceItemTracker
+{
+    public class InvoiceItem
+    {
+        public string Buyer { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Timestamp { get; set; }
+
+        public InvoiceItem()
+        {
+            this.Timestamp = DateTime.Now;
+        }
+
+        public InvoiceItem(DateTime timestamp)
+        {
+            this.Timestamp = timestamp;
+        }
+
+        public bool IsOverdue()
+        {
+            TimeSpan difference = DateTime.Now - this.Timestamp;
+            return (difference.Minutes > 4);
+        }
+    }
+}
